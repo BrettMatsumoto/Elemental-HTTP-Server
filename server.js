a78da0d695cd53ad3780431001a0e898c9d8aa8d
Brett <brett_matsumoto@yahoo.com>
2019-04-22 18:37:58 -1000

    completed authorization

--- server.js
+++ server.js
@@ -3,6 +3,9 @@
 const http = require('http');
 const fs = require('fs');
 const qs = require('querystring');
+const adminPrivilege = {
+  brett: 'password',
+};
 
 const server = http.createServer((req, res) => {
   if (req.method === 'GET') {
@@ -57,218 +60,247 @@ const server = http.createServer((req, res) => {
 
   if (req.method === 'POST') {
     if (req.url === '/elements') {
-      let totalData = '';
+      if (req.headers.authorization) {
+        var encodedString = req.headers.authorization.slice(6);
+        var base64Buffer = new Buffer(encodedString, 'base64');
+        var decodedString = base64Buffer.toString();
+        let username = decodedString.slice(0, decodedString.indexOf(':'));
+        let password = decodedString.slice(decodedString.indexOf(':') + 1);
 
-      req.on('data', (data) => {
-        totalData += data;
-      });
+        if (adminPrivilege.hasOwnProperty(username) && adminPrivilege[username] === password) {
+          let totalData = '';
 
-      req.on('end', () => {
-        const parsedData = qs.parse(totalData);
-
-        const template = `<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-<title>${parsedData.elementName}</title>
-</head>
-<body>
-  <div>${parsedData.elementName}</div>
-  <div>${parsedData.elementSymbol}</div>
-  <div>${parsedData.elementAtomicNumber}</div>
-  <div>${parsedData.elementDescription}</div>
-</body>
-</html>`;
-
-        fs.writeFile(`./public/${parsedData.elementName}.html`, template, 'utf8', (err) => {
-          if (err) {
-            throw err;
-          }
-        });
-
-        fs.readdir('./public', (err, files) => {
-          const knownFiles = files.filter(
-            (file) => file !== '.keep' && file !== '.404' && file !== 'css' && file !== 'index.html',
-          );
-          let tags = '';
-          let lowerCase = '';
-          let standardCase = '';
-          let subString = '';
-
-          for (let newElement = 0; newElement < knownFiles.length; newElement++) {
-            lowerCase = knownFiles[newElement].toLowerCase();
-            standardCase = knownFiles[newElement].charAt(0).toUpperCase() + knownFiles[newElement].slice(1);
-            subString = knownFiles[newElement].indexOf('.');
-            tags += `
-            <li>
-              <a href="${lowerCase}">${standardCase.substring(0, subString)} </a>
-            <li>
-            `;
-          }
-
-          let reWrittenHTML = `
-  <!DOCTYPE html>
-    <html lang="en">
-    <head>
-      <meta charset="UTF-8">
-      <title>The Elements</title>
-      <link rel="stylesheet" href="/css/styles.css">
-    </head>
-    <body>
-      <h1>The Elements</h1>
-      <h2>These are all the known elements.</h2>
-      <h3>These are ${knownFiles.length} elements</h3>
-      <ol>
-      ${tags}
-      </ol>
-    </body>
-    </html>`;
-
-          fs.writeFile(`public/index.html`, reWrittenHTML, 'utf8', (err) => {
-            if (err) {
-              return console.log(err);
-            }
-          });
-        });
-      });
-
-      res.writeHead(200, {
-        'Content-Type': 'application/json',
-      });
-
-      res.end(`{"success" : true}`);
-    }
-  }
-
-  if (req.method === 'PUT') {
-    let totalData = '';
-
-    req.on('data', (data) => {
-      totalData += data;
-    });
-
-    req.on('end', () => {
-      const parsedData = qs.parse(totalData);
-
-      fs.readdir('./public', (err, files) => {
-        const knownFiles = files.filter(
-          (file) => file !== '.keep' && file !== '.404' && file !== 'css' && file !== 'index.html',
-        );
-
-        if (!knownFiles.includes(`${parsedData.elementName}.html`)) {
-          res.writeHead(500, {
-            'Content-Type': 'application/json',
+          req.on('data', (data) => {
+            totalData += data;
           });
 
-          res.end(`{"error"} : ${parsedData.elementName}.html was not found.`);
-        } else {
-          const template = `<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-<title>${parsedData.elementName}</title>
-</head>
-<body>
-  <div>${parsedData.elementName}</div>
-  <div>${parsedData.elementSymbol}</div>
-  <div>${parsedData.elementAtomicNumber}</div>
-  <div>${parsedData.elementDescription}</div>
-</body>
-</html>`;
+          req.on('end', () => {
+            const parsedData = qs.parse(totalData);
 
-          fs.writeFile(`./public/${parsedData.elementName}.html`, template, 'utf8', (err) => {
-            if (err) {
-              throw err;
-            }
+            const template = `<!DOCTYPE html>
+            <html lang="en">
+            <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta http-equiv="X-UA-Compatible" content="ie=edge">
+            <title>${parsedData.elementName}</title>
+            </head>
+            <body>
+            <div>${parsedData.elementName}</div>
+            <div>${parsedData.elementSymbol}</div>
+      <div>${parsedData.elementAtomicNumber}</div>
+      <div>${parsedData.elementDescription}</div>
+      </body>
+      </html>`;
+
+            fs.writeFile(`./public/${parsedData.elementName}.html`, template, 'utf8', (err) => {
+              if (err) {
+                throw err;
+              }
+            });
+
+            fs.readdir('./public', (err, files) => {
+              const knownFiles = files.filter(
+                (file) => file !== '.keep' && file !== '.404' && file !== 'css' && file !== 'index.html',
+              );
+              let tags = '';
+              let lowerCase = '';
+              let standardCase = '';
+              let subString = '';
+
+              for (let newElement = 0; newElement < knownFiles.length; newElement++) {
+                lowerCase = knownFiles[newElement].toLowerCase();
+                standardCase = knownFiles[newElement].charAt(0).toUpperCase() + knownFiles[newElement].slice(1);
+                subString = knownFiles[newElement].indexOf('.');
+                tags += `
+                <li>
+                <a href="${lowerCase}">${standardCase.substring(0, subString)} </a>
+                <li>
+                `;
+              }
+
+              let reWrittenHTML = `
+              <!DOCTYPE html>
+              <html lang="en">
+              <head>
+              <meta charset="UTF-8">
+              <title>The Elements</title>
+              <link rel="stylesheet" href="/css/styles.css">
+              </head>
+        <body>
+        <h1>The Elements</h1>
+        <h2>These are all the known elements.</h2>
+        <h3>These are ${knownFiles.length} elements</h3>
+        <ol>
+        ${tags}
+        </ol>
+        </body>
+        </html>`;
+
+              fs.writeFile(`public/index.html`, reWrittenHTML, 'utf8', (err) => {
+                if (err) {
+                  return console.log(err);
+                }
+              });
+            });
           });
 
           res.writeHead(200, {
             'Content-Type': 'application/json',
           });
 
-          res.end(`{sucess} : true`);
+          res.end(`{"success" : true}`);
         }
-      });
-    });
+      }
+    }
   }
+  if (req.method === 'PUT') {
+    if (req.headers.authorization) {
+      var encodedString = req.headers.authorization.slice(6);
+      var base64Buffer = new Buffer(encodedString, 'base64');
+      var decodedString = base64Buffer.toString();
+      let username = decodedString.slice(0, decodedString.indexOf(':'));
+      let password = decodedString.slice(decodedString.indexOf(':') + 1);
 
-  if (req.method === 'DELETE') {
-    let totalData = '';
+      if (adminPrivilege.hasOwnProperty(username) && adminPrivilege[username] === password) {
+        let totalData = '';
 
-    req.on('data', (data) => {
-      totalData += data;
-    });
-
-    req.on('end', () => {
-      const parsedData = qs.parse(totalData);
-
-      fs.unlink(`./public/${parsedData.elementName}.html`, (err) => {
-        if (err) throw err;
-      });
-
-      if (!`./public/${parsedData.elementName}.html`) {
-        res.writeHead(500, {
-          'Content-Type': 'application/json',
+        req.on('data', (data) => {
+          totalData += data;
         });
 
-        res.end(`{"error" : resource ${parsedData.elementName} does not exist}`);
-      } else {
-        res.writeHead(200, {
-          'Content-Type': 'application/json',
-        });
+        req.on('end', () => {
+          const parsedData = qs.parse(totalData);
 
-        fs.readdir('./public', (err, files) => {
-          const knownFiles = files.filter(
-            (file) => file !== '.keep' && file !== '.404' && file !== 'css' && file !== 'index.html',
-          );
-          let tags = '';
-          let lowerCase = '';
-          let standardCase = '';
-          let subString = '';
+          fs.readdir('./public', (err, files) => {
+            const knownFiles = files.filter(
+              (file) => file !== '.keep' && file !== '.404' && file !== 'css' && file !== 'index.html',
+            );
 
-          for (let newElement = 0; newElement < knownFiles.length; newElement++) {
-            lowerCase = knownFiles[newElement].toLowerCase();
-            standardCase = knownFiles[newElement].charAt(0).toUpperCase() + knownFiles[newElement].slice(1);
-            subString = knownFiles[newElement].indexOf('.');
-            tags += `
-            <li>
-              <a href="${lowerCase}">${standardCase.substring(0, subString)} </a>
-            <li>
-            `;
-          }
+            if (!knownFiles.includes(`${parsedData.elementName}.html`)) {
+              res.writeHead(500, {
+                'Content-Type': 'application/json',
+              });
 
-          let reWrittenHTML = `
-  <!DOCTYPE html>
+              res.end(`{"error"} : ${parsedData.elementName}.html was not found.`);
+            } else {
+              const template = `<!DOCTYPE html>
     <html lang="en">
     <head>
-      <meta charset="UTF-8">
-      <title>The Elements</title>
-      <link rel="stylesheet" href="/css/styles.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>${parsedData.elementName}</title>
     </head>
     <body>
-      <h1>The Elements</h1>
-      <h2>These are all the known elements.</h2>
-      <h3>These are ${knownFiles.length} elements</h3>
-      <ol>
-      ${tags}
-      </ol>
+    <div>${parsedData.elementName}</div>
+    <div>${parsedData.elementSymbol}</div>
+    <div>${parsedData.elementAtomicNumber}</div>
+    <div>${parsedData.elementDescription}</div>
     </body>
     </html>`;
 
-          fs.writeFile(`public/index.html`, reWrittenHTML, 'utf8', (err) => {
-            if (err) {
-              return console.log(err);
+              fs.writeFile(`./public/${parsedData.elementName}.html`, template, 'utf8', (err) => {
+                if (err) {
+                  throw err;
+                }
+              });
+
+              res.writeHead(200, {
+                'Content-Type': 'application/json',
+              });
+
+              res.end(`{sucess} : true`);
             }
           });
         });
-
-        res.end(`{sucess} : true`);
       }
-    });
+    }
+  }
+
+  if (req.method === 'DELETE') {
+    if (req.headers.authorization) {
+      var encodedString = req.headers.authorization.slice(6);
+      var base64Buffer = new Buffer(encodedString, 'base64');
+      var decodedString = base64Buffer.toString();
+      let username = decodedString.slice(0, decodedString.indexOf(':'));
+      let password = decodedString.slice(decodedString.indexOf(':') + 1);
+
+      if (adminPrivilege.hasOwnProperty(username) && adminPrivilege[username] === password) {
+        let totalData = '';
+
+        req.on('data', (data) => {
+          totalData += data;
+        });
+
+        req.on('end', () => {
+          const parsedData = qs.parse(totalData);
+
+          fs.unlink(`./public/${parsedData.elementName}.html`, (err) => {
+            if (err) throw err;
+          });
+
+          if (!`./public/${parsedData.elementName}.html`) {
+            res.writeHead(500, {
+              'Content-Type': 'application/json',
+            });
+
+            res.end(`{"error" : resource ${parsedData.elementName} does not exist}`);
+          } else {
+            res.writeHead(200, {
+              'Content-Type': 'application/json',
+            });
+
+            fs.readdir('./public', (err, files) => {
+              const knownFiles = files.filter(
+                (file) => file !== '.keep' && file !== '.404' && file !== 'css' && file !== 'index.html',
+              );
+              let tags = '';
+              let lowerCase = '';
+              let standardCase = '';
+              let subString = '';
+
+              for (let newElement = 0; newElement < knownFiles.length; newElement++) {
+                lowerCase = knownFiles[newElement].toLowerCase();
+                standardCase = knownFiles[newElement].charAt(0).toUpperCase() + knownFiles[newElement].slice(1);
+                subString = knownFiles[newElement].indexOf('.');
+                tags += `
+                <li>
+                <a href="${lowerCase}">${standardCase.substring(0, subString)} </a>
+                <li>
+                `;
+              }
+
+              let reWrittenHTML = `
+              <!DOCTYPE html>
+              <html lang="en">
+              <head>
+              <meta charset="UTF-8">
+              <title>The Elements</title>
+              <link rel="stylesheet" href="/css/styles.css">
+              </head>
+              <body>
+              <h1>The Elements</h1>
+              <h2>These are all the known elements.</h2>
+              <h3>These are ${knownFiles.length} elements</h3>
+              <ol>
+              ${tags}
+              </ol>
+              </body>
+              </html>`;
+
+              fs.writeFile(`public/index.html`, reWrittenHTML, 'utf8', (err) => {
+                if (err) {
+                  return console.log(err);
+                }
+              });
+            });
+
+            res.end(`{sucess} : true`);
+          }
+        });
+      }
+    }
   }
 });
 server.listen(8080);
